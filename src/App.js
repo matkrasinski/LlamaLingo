45ca184b207ced26b204118fcce75233e5e9fc16
Ryszawy <Krafi604@gmail.com>
2025-01-04 15:32:16 +0100

    feat: home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Login from "./components/auth/login";
 import Register from "./components/auth/register";
 
-import Header from "./components/header";
 import Home from "./components/home";
 
 import { AuthProvider } from "./contexts/authContext";
@@ -29,7 +28,6 @@ function App() {
   let routesElement = useRoutes(routesArray);
   return (
     <AuthProvider>
-      <Header />
       <div className="w-full h-screen flex flex-col">{routesElement}</div>
     </AuthProvider>
   );
