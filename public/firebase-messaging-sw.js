8b20a8e8b8bd63af401e9ef1780c704b2b94f82e
Jakub Czyżewski <jakub.czyzewski@tomtom.com>
2025-01-23 18:43:04 +0100

    get token and create service worker

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,27 @@
+importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js');
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+const messaging = getMessaging(app);
+
+onBackgroundMessage(messaging, (payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: payload.notification.icon
+    };
+
+showNotification(self.registration, notificationTitle, notificationOptions);
+});
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,6 +5,7 @@ import { loadSampleTasks } from "./sample/tasks";
 import { loadSampleLessons } from "./sample/lessons";
 import { loadCoursesToFirestore } from "../utils/units"
 import { getDocsFromCollection, setDocFromCollection } from "./db";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
 
 const firebaseConfig = {
@@ -21,6 +22,26 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
 const db = getFirestore(app);
+const messaging = getMessaging(app);
+
+console.log('Requesting permission...');
+Notification.requestPermission().then((permission) => {
+  console.log(permission)
+  if (permission === 'granted') {
+    console.log('Notification permission granted.');
+    return getToken(messaging, { vapidKey: 'BARKrFULp2YR-Vox_G9QVwzfGrmH6ZoBD82w0GhrvwZIfLL7xagVNdtB-OmNrfLJFlYzptKprsP9AwDYuV35-tM'})
+  } else {
+    console.log('permission not granted: ' + permission)
+  }
+}).then((token) => {
+  console.log(token);
+}).catch((err) => {
+  console.log('error ocurred: ' + err)
+})
+
+onMessage(messaging, (payload) => {
+  console.log('onMessage: ', payload)
+})
 
 // console.log(await getDocsFromCollection("courses"))
 // await loadCoursesToFirestore(db)
@@ -29,4 +50,4 @@ const db = getFirestore(app);
 
 
 
-export { app, auth, db };
+export { app, auth, db, messaging };
