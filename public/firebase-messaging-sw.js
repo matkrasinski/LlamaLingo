4e6e54225f75f8dca8f5b378a5e7f5c92399025f
Mateusz <mat.krasinski@o2.pl>
2025-01-26 17:13:02 +0100

    Feat/updates (#29)
    * feat: ToastContainer missing import
    
    * feat: smooth updates

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,13 +4,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging-comp
 
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID,
-  measurementId: process.env.REACT_APP_MEASUREMENTID,
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 firebase.initializeApp(firebaseConfig);
