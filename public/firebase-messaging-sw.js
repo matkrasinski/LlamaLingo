f572239574966dfdeee8995e00b8b7dfa0841e69
matkrasinski <mat.krasinski@o2.pl>
2025-01-26 22:40:13 +0100

    feat: notf

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/9.10.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging-compat.js');
+importScripts("/service-worker.js")
 
 
 const firebaseConfig = {
