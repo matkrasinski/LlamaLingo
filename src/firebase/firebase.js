73f8cc2bd154bfdc6367c6e977144aa876f5fa80
Mateusz <mat.krasinski@o2.pl>
2025-01-26 20:35:05 +0100

    feat: FCM condition (#33)

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getToken, getMessaging, onMessage, isSupported } from 'firebase/messaging';
+import {
+  getToken,
+  getMessaging,
+  onMessage,
+  isSupported,
+} from "firebase/messaging";
 import { getDocs, getFirestore } from "firebase/firestore";
 import { loadSampleTasks } from "./sample/tasks";
 import { loadSampleLessons } from "./sample/lessons";
-import { loadCoursesToFirestore } from "../utils/units"
+import { loadCoursesToFirestore } from "../utils/units";
 import { getDocsFromCollection, setDocFromCollection } from "./db";
-
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -17,14 +21,17 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APPID,
   measurementId: process.env.REACT_APP_MEASUREMENTID,
 };
-console.log('*** Environment ***', process.env.REACT_APP_ENV)
-console.log('*** Firebase Config ***', firebaseConfig)
-
+console.log("*** Environment ***", process.env.REACT_APP_ENV);
+console.log("*** Firebase Config ***", firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
+const auth = getAuth(app);
 const db = getFirestore(app);
-const messaging = getMessaging(app);
+
+let isFCMSupported = await isSupported();
+console.log("is FCM supported? :", isFCMSupported);
+
+const messaging = isFCMSupported ? getMessaging(app) : null;
 
 // console.log(await getDocsFromCollection("courses"))
 // await loadCoursesToFirestore(db)
@@ -32,28 +39,31 @@ const messaging = getMessaging(app);
 // await loadSampleLessons(db)
 
 export const getOrRegisterServiceWorker = () => {
-  if ('serviceWorker' in navigator) {
+  if ("serviceWorker" in navigator) {
     return window.navigator.serviceWorker
-      .getRegistration('/firebase-push-notification-scope')
+      .getRegistration("/firebase-push-notification-scope")
       .then((serviceWorker) => {
         if (serviceWorker) return serviceWorker;
-        return window.navigator.serviceWorker.register('/firebase-messaging-sw.js', {
-          scope: '/firebase-push-notification-scope',
-        });
+        return window.navigator.serviceWorker.register(
+          "/firebase-messaging-sw.js",
+          {
+            scope: "/firebase-push-notification-scope",
+          }
+        );
       });
   }
-  throw new Error('The browser doesn`t support service worker.');
+  throw new Error("The browser doesn`t support service worker.");
 };
 
-
 export const getFirebaseToken = () =>
-  getOrRegisterServiceWorker()
-    .then((serviceWorkerRegistration) =>
-      getToken(messaging, { vapidKey: process.env.REACT_APP_VAPID_KEY, serviceWorkerRegistration }));
+  getOrRegisterServiceWorker().then((serviceWorkerRegistration) =>
+    getToken(messaging, {
+      vapidKey: process.env.REACT_APP_VAPID_KEY,
+      serviceWorkerRegistration,
+    })
+  );
 
 export const onForegroundMessage = () =>
   new Promise((resolve) => onMessage(messaging, (payload) => resolve(payload)));
-
-
 
 export { app, auth, db };
