3149c18932fad365e1bc6ea7bac1453727c8c34d
matkrasinski <mat.krasinski@o2.pl>
2025-01-27 16:43:03 +0100

    feat: cache + notifications

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/9.10.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging-compat.js');
+importScripts("/service-worker.js")
 
 
 const firebaseConfig = {
@@ -21,7 +22,9 @@ messaging.onBackgroundMessage((payload) => {
   console.log('Received background message: ', payload);
 
   const notificationTitle = payload.notification.title;
-  const notificationOptions = { body: payload.notification.body };
+  const notificationOptions = { 
+    body: payload.notification.body,
+  };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
